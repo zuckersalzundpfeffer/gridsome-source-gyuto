0eaf9f9b305d1fe4182b90e7cfecede44c602113
Boris Brue <boris@zuckersalzundpfeffer.de>
2022-07-11 10:11:29 +0200

    changes on the source

--- helpers/index.js
+++ helpers/index.js
@@ -2,16 +2,41 @@ const GYUTO_URL = "https://gyuto.uber.space";
 
 const getGyutoMediaPath = (relativePath, type, srv = null) => {
   const server = srv ? srv : GYUTO_URL;
-  return relativePath.replace(`original_${type}`, `${server}/media`);
+  return relativePath.replace(`original_${type}`, `${server}/media/${type}`);
 };
 const test = getGyutoMediaPath("original_images/foo/bar.jpg", "images");
 test;
 
-const getGyutoImage = ({ file }, host) => {
-  return getGyutoMediaPath(file, "images", host);
+const getGyutoImage = ({ file }, options, host) => {
+  let fileName = file;
+  const widthOption = options.width ? `width-${options.width}` : null;
+  const formatOption = options.format ? `format-${options.format}` : null;
+  const type = "images";
+  const filePropString = "." + [widthOption, formatOption].join(".");
+  filePropString;
+  const params = { type, filePropString };
+  params;
+  if (options) {
+    const array = file.split(".");
+    array;
+    let mimeType = array[array.length - 1];
+    mimeType;
+    if (options.format) {
+      mimeType = options.format;
+    }
+    fileName = array[0] + filePropString + "." + mimeType;
+  }
+
+  const serverFile = getGyutoMediaPath(fileName, "images", host);
+  serverFile;
+  return serverFile;
 };
 
-const test_img = getGyutoImage({ file: "original_images/blub.jpg" }, "https://test01.gyuto.uber.space");
+const test_img = getGyutoImage(
+  { file: "original_images/blub.jpg" },
+  { width: 900, format: "webp" },
+  "https://test01.gyuto.uber.space"
+);
 test_img;
 
 module.exports = {
